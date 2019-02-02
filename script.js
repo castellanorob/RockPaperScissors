22934709354515885415f42199d30477e0ed7ced
castellanorob <castellanorob@gmail.com>
2019-02-02 13:25:59 -0600

    Add content to html and js

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("I am linked to the html file!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Set the inner HTML contents of the #game div to our html string
+document.querySelector("#game").innerHTML = html;
